d36ca0723e02fc867c88071687c2f7c48ed24710
ujrye <105918178+WindfuryKun@users.noreply.github.com>
2025-12-29 21:11:26 +0800

    initialize project

---
+++ src/main/java/team/lightcat/catdown/CatDownApplication.java
@@ -0,0 +1,13 @@
+package team.lightcat.catdown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatDownApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatDownApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team/lightcat/catdown/CatDownApplicationTests.java
@@ -0,0 +1,13 @@
+package team.lightcat.catdown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatDownApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
